f75593bf75bd44ca1c4eee40fbf13c7872644858
Tizian Bluntschli <tizian.bluntschli@cern.ch>
2016-03-21 11:12:33 +0100

    HadronFlavour included

--- test/GaussianFit.py
+++ test/GaussianFit.py
@@ -2,6 +2,7 @@ import numpy as np
 import ROOT
 import matplotlib.pyplot as plt
 
+#Open the raw data (TTree)
 RootFile  = ROOT.TFile( 'Egen_to_Ereco.root' )
 Tree = ROOT.gDirectory.Get('Egen_to_Ereco')
 entries = Tree.GetEntries()
@@ -18,76 +19,77 @@ regions.append(eta_1)
 regions.append(eta_2)
 regions.append(eta_3)
 
+HadronFlavours = []
+HadronFlavours.append(0)
+HadronFlavours.append(5)
+
 #Create ROOT File to write Fits & Histograms to, w a Subdirectory for each Eta region
 Saved_Fits = ROOT.TFile( 'Egen_to_Ereco_fits.root', 'RECREATE')
 Saved_Fits.cd()
+
+eta_directories = []
+
 for region in range(len(regions)):
-    Saved_Fits.mkdir("eta" + str(region), "Eta region between " + str(regions[region][0]) + " and " + str(regions[region][1]))
-
-#Decomment this if you want to check if the data read from the ROOT file is sensible (it will print every 10000th entry)
-#for entry in xrange(entries):
-#    ientry = Tree.LoadTree(entry)
-#    nb = Tree.GetEntry(entry)
-#    if entry%10000 == 0:
-#        print "Entry ", entry, "of ", entries
-#        print "GenPt value: ", Tree.GenPt
-#        print "RecoPt value: ", Tree.RecoPt
-#        print "nJet value: ", Tree.nJet
-
+    eta_dir = Saved_Fits.mkdir("eta" + str(region), "Eta region between " + str(regions[region][0]) + " and " + str(regions[region][1]))
+    eta_directories.append(eta_dir)
+    for flavour in HadronFlavours:
+        eta_dir.cd()
+        flavour_dir = eta_dir.mkdir("Flavour"+str(flavour), "Flavour == "+str(flavour))
 no_fits = 15
 interval_width = 10
 fit_params = np.zeros((no_fits,2,3,len(regions)))
 
 for region in range(len(regions)):
-    #Go into directory of current eta region
-    Saved_Fits.cd("eta" + str(region))
-    for i in xrange(no_fits):
-    #Initialize Interval in which Gaussian Fit will be made
-        interval = [(i+2)*interval_width, (i+3)*interval_width]
 
-    #Create the strings that limit the data to the range of the Eta & RecoPt intervals
+    for flavour in HadronFlavours:
 
-        string1 = []
-        string1.append('GenPt>>h_')
-        string1.append(str(i))
-        string1 = "".join(string1)
+        #Go into directory of eta region, then into flavour directory
+        eta_directories[region].cd("Flavour"+str(flavour))
 
-        string2 = []
-        string2.append('RecoPt > ')
-        string2.append(str(interval[0]))
-        string2.append(' && RecoPt < ')
-        string2.append(str(interval[1]))
-        string2.append(' && RecoEta > ')
-        string2.append(str(regions[region][0]))
-        string2.append(' && RecoEta < ')
-        string2.append(str(regions[region][1]))
-        string2 = "".join(string2)
-        print string2
+        for i in xrange(no_fits):
+            #Initialize Interval in which Gaussian Fit will be made
+            interval = [(i+2)*interval_width, (i+3)*interval_width]
 
-        substr = []
-        substr.append('h_')
-        substr.append(str(i))
-        substr = "".join(substr)
-        print substr
+            #Create the strings that limit the data to the range of the Eta & RecoPt intervals & to the current Hadron Flavour
+
+            string1 = []
+            string1.append('GenPt>>h_')
+            string1.append(str(i))
+            string1 = "".join(string1)
+
+            string2 = []
+            string2.append('RecoPt > ')
+            string2.append(str(interval[0]))
+            string2.append(' && RecoPt < ')
+            string2.append(str(interval[1]))
+            string2.append(' && RecoEta > ')
+            string2.append(str(regions[region][0]))
+            string2.append(' && RecoEta < ')
+            string2.append(str(regions[region][1]))
+            string2.append(' && GenFlavour == ')
+            string2.append(str(flavour))
+            string2 = "".join(string2)
+
+            substr = []
+            substr.append('h_')
+            substr.append(str(i))
+            substr = "".join(substr)
     
-    #Create Histogram in Histo, fit it to a Gaussian in param and save the three parameters to fit_params (and their errors)
-        test = Tree.Draw(string1, string2)
-        Histo = ROOT.gDirectory.Get(substr)
-        param = Histo.Fit("gaus", "S")
-        Histo.Write()
+            #Create Histogram in Histo, fit it to a Gaussian in param and write the Histogram to the root file opened in the start
+            test = Tree.Draw(string1, string2)
+            Histo = ROOT.gDirectory.Get(substr)
+            param = Histo.Fit("gaus", "S")
+            Histo.Write()
+        #Go back to the current Eta Directory
+        eta_directories[region].cd()
 
-        fit_params[i,0,0,region] = param.Value(0) 
-        fit_params[i,0,1,region] = param.Value(1)
-        fit_params[i,0,2,region] = param.Value(2)
-        fit_params[i,1,0,region] = param.Error(0)
-        fit_params[i,1,1,region] = param.Error(1)
-        fit_params[i,1,2,region] = param.Error(2)
+            #fit_params[i,0,0,region] = param.Value(0) 
+            #fit_params[i,0,1,region] = param.Value(1)
+            #fit_params[i,0,2,region] = param.Value(2)
+            #fit_params[i,1,0,region] = param.Error(0)
+            #fit_params[i,1,1,region] = param.Error(1)
+            #fit_params[i,1,2,region] = param.Error(2)
 
-#print fit_params[:,0,:,0]
-#print fit_params[:,0,:,1]
-
-##Create json object that contains the eta regions, number of fits & interval width 
-#meta_data = {'eta0': eta_0, 'eta1': eta_1, 'eta2': eta_2, 'eta3': eta_3, 'no_fits': no_fits, 'interval_width': interval_width} 
 
 Saved_Fits.ls()
 Saved_Fits.Close()
--- test/SigmasFit.py
+++ test/SigmasFit.py
@@ -5,14 +5,26 @@ import re
 RootFile = ROOT.TFile('Egen_to_Ereco_fits.root')
 
 #Some shenanigans to extract the number of Eta regions and the number of fits in each region from the RootFile
+
 ListOfKeys = RootFile.GetListOfKeys()
 first_key = ListOfKeys[0]
 name_of_first_key = first_key.GetName()
 first_directory = RootFile.GetDirectory(name_of_first_key)
-Keys_in_first_Directory = first_directory.GetListOfKeys()
 
-no_fits = Keys_in_first_Directory.GetEntries()
+first_directory.cd()
+
+Keys_in_first_Directory = first_directory.GetListOfKeys()
+first_key_in_eta_dir = Keys_in_first_Directory[0]
+name_of_first_key_in_eta_dir = first_key_in_eta_dir.GetName()
+first_directory_in_eta_dir = first_directory.GetDirectory(name_of_first_key_in_eta_dir)
+
+Keys_in_first_eta_dir = first_directory_in_eta_dir.GetListOfKeys()
+
+no_fits = Keys_in_first_eta_dir.GetEntries()
 no_regions = ListOfKeys.GetEntries()
+
+print "no_fits: ", no_fits
+print "no_regions: ", no_regions
 
 #Extract the actual Eta region boundaries from the titles of the directories of the RootFile to save them later
 regions = []
@@ -34,35 +46,52 @@ for i in xrange(no_regions):
 result ={}
 
 for i in xrange(no_regions):
-    RootFile.cd("eta"+str(i))
-    Test = ROOT.TH1F("Sigmas", "Fitted Sigma values for Eta in ["+str(regions[i][0])+","+str(regions[i][1])+"]" , 320000, 15.0, 200.0)
+    current_eta_dir = RootFile.GetDirectory("eta"+str(i))
+    for flavour in [0,5]:
+        current_eta_dir.cd("Flavour"+str(flavour))
+        Test = ROOT.TH1F("Sigmas", "Fitted Sigma values for Eta in ["+str(regions[i][0])+","+str(regions[i][1])+"]" +" and HadronFlavour == "+str(flavour) , 320000, 15.0, 200.0)
 
-    for fit in xrange(no_fits):
+        for fit in xrange(no_fits):
         #Load Histogram h_fit and from it load the Gaussian function fitted to it
-        Histo = ROOT.gDirectory.Get("h_"+str(fit))
-        myfunc = Histo.GetFunction("gaus")   
+            Histo = ROOT.gDirectory.Get("h_"+str(fit))
+            myfunc = Histo.GetFunction("gaus")   
 
         #Parameter 1 and 2 correspond to the position and the width of the Gaussian fit
-        current_sigma = myfunc.GetParameter(2)
-        current_energy = myfunc.GetParameter(1)
+            current_sigma = myfunc.GetParameter(2)
+            current_energy = myfunc.GetParameter(1)
 
         #Find bin with value of position of the Gaussian and write Gaussian width to it
-        bin_number = Test.FindBin(current_energy)
-        Test.SetBinContent(bin_number, current_sigma)
+            bin_number = Test.FindBin(current_energy)
+            Test.SetBinContent(bin_number, current_sigma)
     #This Function is not known if defined at the very start of the macro. No idea why.     
-    myfunc = ROOT.TF1("myfunc", "[0] + [1]*x + [2]*sqrt(x)", 20, 200)
-    params = Test.Fit("myfunc", "S")
-    result["eta"+str(i)] = [[params.Value(0), params.Value(1), params.Value(2)],regions[i]]
-    
+        myfunc = ROOT.TF1("myfunc", "[0]*x + [1]*sqrt(x) + [2]", 20, 200)
+        params = Test.Fit("myfunc", "S")
+        result["eta"+str(i),flavour] = [[params.Value(0), params.Value(1), params.Value(2)],regions[i], flavour]
+
 Test.SetMarkerColor(2)
 Test.SetMarkerSize(1)
 Test.SetMarkerStyle(5)
 Test.Draw("P")
 
-print result['eta0']
-print result['eta1']
-print result['eta2']
-print result['eta3']
+print "Hey man check out the eta0 results :"
+print result["eta0", 0]
+print result["eta0", 5]
+print " "
+print " "
+print "Also eta1 is a special kind of nice :"
+print result["eta1", 0]
+print result["eta1", 5]
+print " "
+print " "
+print "Have you seen eta2 though? Whew :"
+print result["eta2", 0]
+print result["eta2", 5]
+print " "
+print " "
+print "At eta3 we have some real gems as well :"
+print result["eta3", 0]
+print result["eta3", 5]
+
 
 
 Bla = raw_input()
