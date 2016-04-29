d7a27b06aa9fb1d65f9787d3c47a57009036fec8
Tizian Bluntschli <tizian.bluntschli@cern.ch>
2016-04-29 10:47:43 +0200

    Back up

--- test_1/BukinFit.py
+++ test_1/BukinFit.py
@@ -2,15 +2,21 @@ import ROOT
 import numpy as np
 ROOT.gSystem.Load("libRooFit")
 
+bin_number = 35
+x_boundary = 75
+y_boundary = 165
+
 labeler = 0
 some_values = []
 for no_jets in ["==2","==3","==4", ">4"]:
 
+    bla = ROOT.RooDataHist()
+
     labeler += 1
     c1 = ROOT.TCanvas("c1", "c1", 800,800)
-    leg = ROOT.TLegend(0.55, 0.65, 0.9, 0.9)
+    leg = ROOT.TLegend(0.68, 0.65, 0.9, 0.9)
  
-    x = ROOT.RooRealVar("x-axis", "x", 0.0 , 250.0)
+    x = ROOT.RooRealVar("x-axis", "x", 75.0 , 165.0)
     Xp = ROOT.RooRealVar("Xp_sgn", "Xp",100.0 , 200.0)
     sP = ROOT.RooRealVar("sP_sgn", "sP", 0.0 , 50.0)
     xi = ROOT.RooRealVar("xi_sgn", "xi", -5.0, 5.0)
@@ -25,55 +31,86 @@ for no_jets in ["==2","==3","==4", ">4"]:
     mean_corrfac_sigma = []
     iteration_counter = 0
 
-    for mass in ["est_mass", "mea_mass", "(est_mass*(est_mass > 0) + 2*(est_mass == 0)*mea_mass + (est_mass > 0)*mea_mass)/2"]:
+    for mass in ["est_mass", "mea_mass", "superposition"]:
         
         data = ROOT.TFile('ChiSquareFits_NeutrinoAdded_Full_only_Flavour5_hJCidx.root')
         Tree = ROOT.gDirectory.Get('ChiSquareFits')
         
-        if mass != "est_mass":
-            Tree.Draw(mass+">>h1(100,0,500)", "nJet" + no_jets )
+        if mass == "mea_mass":
+            Tree.Draw(mass+">>h1(" +str(bin_number) + "," + str(x_boundary) + "," + str(y_boundary) + ")", "nJet" + no_jets )
         
-        Tree.Draw(mass+">>h1(100,0,500)", "nJet" + no_jets + "&& est_mass != 0")
-        
-        
-        h1 = ROOT.gDirectory.Get("h1")
-        Roo_histo_1 = ROOT.RooDataHist("higgs_mass_estimate", "estimated higgs mass",  ROOT.RooArgList(x), h1, 1.0)
-        RooBukinPdf.fitTo(Roo_histo_1)
+        if mass == "est_mass":
+            Tree.Draw(mass+">>h1(" +str(bin_number) + "," + str(x_boundary) + "," + str(y_boundary) + ")", "nJet" + no_jets )
 
-        if iteration_counter != 2:
+        if iteration_counter in [0,1]:        
+            h1 = ROOT.gDirectory.Get("h1")
+            Roo_histo_1 = ROOT.RooDataHist("higgs_mass_estimate", "estimated higgs mass",  ROOT.RooArgList(x), h1, 1.0)
+            RooBukinPdf.fitTo(Roo_histo_1)
             mean_corrfac_sigma.append(Xp.getVal())
             mean_corrfac_sigma.append(sP.getVal())
 
         if iteration_counter == 2:
-            Tree.Draw("est_mass>>est_histo(100,0,250)", "est_mass != 0 && est_mass < 250")
-            Tree.Draw("mea_mass>>mea_histo(100,0,250)", "est_mass < 250")
-            Tree.Draw("est_mass:mea_mass>>two_dim_histo", "est_mass != 0 && est_mass < 250", "COLZ")
-            two_dim_histo = ROOT.gDirectory.Get("two_dim_histo")
+            
+            #Extracting the correlation factor from the est_mass & mea_mass diagrams
+            Tree.Draw("est_mass>>est_histo(" +str(bin_number) + "," + str(x_boundary) + "," + str(y_boundary) + ")", "est_mass < 250 && nJet" + no_jets)
+            Tree.Draw("mea_mass>>mea_histo(" +str(bin_number) + "," + str(x_boundary) + "," + str(y_boundary) + ")", "est_mass < 250 && nJet" + no_jets)
             est_histo = ROOT.gDirectory.Get("est_histo")
             mea_histo = ROOT.gDirectory.Get("mea_histo")
-            corr_fac = two_dim_histo.GetCorrelationFactor()
-            mean_corrfac_sigma.append(corr_fac)
-
-            overall_factor = 1.0/(1.0 / mean_corrfac_sigma[1]**2 + 2.0*corr_fac/(mean_corrfac_sigma[1]*mean_corrfac_sigma[3]) + 1.0 / mean_corrfac_sigma[3]**2)
-            est_factor = (1.0/mean_corrfac_sigma[1]**2 - corr_fac/(mean_corrfac_sigma[1]*mean_corrfac_sigma[3]))*overall_factor
-            constant = mean_corrfac_sigma[0]/mean_corrfac_sigma[1]**2 + mean_corrfac_sigma[2]/mean_corrfac_sigma[3]**2 - (mean_corrfac_sigma[2]+mean_corrfac_sigma[0])*corr_fac/(mean_corrfac_sigma[1]*mean_corrfac_sigma[3])
-            mea_factor = (1.0/mean_corrfac_sigma[3]**2 - corr_fac/(mean_corrfac_sigma[1]*mean_corrfac_sigma[3]))*overall_factor
+        
+            upperbound_est = mean_corrfac_sigma[0] + mean_corrfac_sigma[1]
+            lowerbound_est = mean_corrfac_sigma[0] - mean_corrfac_sigma[1]
             
+            upperbound_mea = mean_corrfac_sigma[2] + mean_corrfac_sigma[3]
+            lowerbound_mea = mean_corrfac_sigma[2] - mean_corrfac_sigma[3]
+
+            string_2d = "est_mass > " + str(lowerbound_est) + "&& est_mass <" + str(upperbound_est) + "&& mea_mass > " + str(lowerbound_mea) + "&& mea_mass < " + str(upperbound_mea)
+            Tree.Draw("est_mass:mea_mass>>two_dim_histo", "est_mass != 0 &&" + string_2d + " && nJet" + no_jets, "COLZ")
+            two_dim_histo = ROOT.gDirectory.Get("two_dim_histo")
+            corr_fac = two_dim_histo.GetCorrelationFactor()
+ #           print corr_fac
+#            a = raw_input()
+            mean_corrfac_sigma.append(corr_fac)
+            
+            #Calculate the factors for the superposition of the est & mea diagrams
+
+            overall_factor = 1.0/(mean_corrfac_sigma[1]**2 - 2.0*corr_fac*mean_corrfac_sigma[1]*mean_corrfac_sigma[3] + mean_corrfac_sigma[3]**2)
+            est_factor = (mean_corrfac_sigma[1]**2 - 2.0*corr_fac*mean_corrfac_sigma[1]*mean_corrfac_sigma[3])*overall_factor
+            mea_factor = (mean_corrfac_sigma[3]**2 - 2.0*corr_fac*mean_corrfac_sigma[1]*mean_corrfac_sigma[3])*overall_factor
+            constant = overall_factor*est_factor*(mean_corrfac_sigma[2] - mean_corrfac_sigma[0])
+#            overall_factor = 1.0/(1.0 / mean_corrfac_sigma[1]**2 + 2.0*corr_fac/(mean_corrfac_sigma[1]*mean_corrfac_sigma[3]) + 1.0 / mean_corrfac_sigma[3]**2)
+#            est_factor = (1.0/mean_corrfac_sigma[1]**2 - corr_fac/(mean_corrfac_sigma[1]*mean_corrfac_sigma[3]))*overall_factor
+#            constant = mean_corrfac_sigma[0]/mean_corrfac_sigma[1]**2 + mean_corrfac_sigma[2]/mean_corrfac_sigma[3]**2 - (mean_corrfac_sigma[2]+mean_corrfac_sigma[0])*corr_fac/(mean_corrfac_sigma[1]*mean_corrfac_sigma[3])
+#            mea_factor = (1.0/mean_corrfac_sigma[3]**2 - corr_fac/(mean_corrfac_sigma[1]*mean_corrfac_sigma[3]))*overall_-factor
+
+
             est_histo.Scale(est_factor)
-            no_bins = est_histo.GetNbinsX()
-            no_bins = int(no_bins)
+            no_bins = int(est_histo.GetNbinsX())
             for i in xrange(no_bins):
                 est_histo.AddBinContent(i, constant)
 
             est_histo.Add(mea_histo, mea_factor)
-            est_histo.Draw()
-            a = raw_input("Check this out yo")
 
-            bla = ROOT.RooDataHist("higgs_mass_estimate", "estimated higgs mass",  ROOT.RooArgList(x), est_histo, 1.0)
-            Roo_histo_1 = bla
-            RooBukinPdf.fitTo(Roo_histo_1)
+            bla = ROOT.RooDataHist("higgs_mass_estimate", "Weighted superposition of est_mass & mea_mass",  ROOT.RooArgList(x), est_histo, 1.0)
+
+#            print est_factor, constant, mea_factor
+
+#            a = raw_input("Fit about to start" + str(bla.numEntries()))
+
+            RooBukinPdf.fitTo(bla)
+
+#            c1 = ROOT.TCanvas("c1", "c1", 800,800)
+#            frame = x.frame()
+#            frame.SetName("frame")
+#            frame.SetTitle("Higgs mass estimate w. a Bukin function fitted to it for nJet " + no_jets)
             
+#            RooBukinPdf.plotOn(frame)
+#            bla.plotOn(frame)
 
+#            c1.cd()
+#            frame.Draw()
+
+#            a = raw_input("We done")
+            
         some_values.append(Xp.getVal())
         some_values.append(sP.getVal())
 
@@ -85,16 +122,23 @@ for no_jets in ["==2","==3","==4", ">4"]:
         if mass == "mea_mass":
             color = ROOT.RooFit.LineColor(3)
             style_histo = ROOT.RooFit.MarkerStyle(2)
+
         if iteration_counter == 2:
             color = ROOT.RooFit.LineColor(4)
             style_histo = ROOT.RooFit.MarkerStyle(4)
 
+        if iteration_counter != 2:
+            Roo_histo_1.plotOn(frame, color)
+        else:
+            bla.plotOn(frame,color)
 
-        Roo_histo_1.plotOn(frame, color)
+        cur_mean = str(Xp.getVal())
+        cur_sig = str(sP.getVal())
+
         RooBukinPdf.plotOn(frame, color, name)
-
         leg.AddEntry(frame.getCurve(mass), mass)
-    
+        leg.AddEntry(frame.getCurve(mass), "mean = " + cur_mean)
+        leg.AddEntry(frame.getCurve(mass), "sigma = " + cur_sig)
         iteration_counter += 1
 
     data.Close()
@@ -105,7 +149,7 @@ for no_jets in ["==2","==3","==4", ">4"]:
 #    ROOT.gDirectory.Delete("buk_pdf_sgn")
 #    ROOT.gDirectory.Delete("x-axis")
 
-    leg.SetTextSize(0.02)
+    leg.SetTextSize(0.015)
     c1.cd()
     frame.Draw()
     leg.Draw()
